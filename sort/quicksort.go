ed0ec7938f0f1c4079dca106f428e33c0275c089
Lyu <lvliangxiong@foxmail.com>
2025-02-15 11:55:48 +0800

    sort: add quick select algorithm

--- sort/quicksort.go
+++ sort/quicksort.go
@@ -1,11 +1,16 @@
 package sort
 
-import "math/rand"
+import (
+	"fmt"
+	"math/rand"
+)
 
 type Quick struct{}
 
 // Sort adopts an tree-way partition algorithm, which is effective for sorting
 // array containing lots of duplicate elements.
+//
+// Relevant leetcode problem: https://leetcode.cn/problems/sort-an-array/description/.
 func (quick *Quick) Sort(nums []int) {
 	if len(nums) <= 1 {
 		return
@@ -26,7 +31,7 @@ func (quick *Quick) Sort(nums []int) {
 			lt++
 			nums[lt], nums[i] = nums[i], nums[lt]
 			i++
-		} else if nums[i] > pivot {
+		} else {
 			gt--
 			nums[gt], nums[i] = nums[i], nums[gt]
 		}
@@ -67,4 +72,43 @@ func (quick *Quick) TwoWaySort(nums []int) {
 	// nums[j] is the partition location.
 	quick.TwoWaySort(nums[0:j])
 	quick.TwoWaySort(nums[j+1:])
+}
+
+// Select find the k-th largest element in nums.
+//
+// Relevant leetcode problem: https://leetcode.cn/problems/kth-largest-element-in-an-array/.
+func (quick *Quick) Select(nums []int, k int) int {
+	if k <= 0 || k > len(nums) {
+		panic(fmt.Sprintf("unexpected k = %d", k))
+	}
+
+	if len(nums) == 1 {
+		return nums[0]
+	}
+
+	randIdx := rand.Intn(len(nums))
+	nums[0], nums[randIdx] = nums[randIdx], nums[0]
+
+	lt, gt, i, pivot := -1, len(nums), 1, nums[0]
+	for i < gt {
+		if nums[i] == pivot {
+			i++
+		} else if nums[i] < pivot {
+			lt++
+			nums[lt], nums[i] = nums[i], nums[lt]
+			i++
+		} else {
+			gt--
+			nums[gt], nums[i] = nums[i], nums[gt]
+		}
+	}
+
+	// Element in range (le, gt) equals to pivot.
+	if len(nums)-k <= lt {
+		return quick.Select(nums[0:lt+1], k+lt-len(nums)+1)
+	}
+	if len(nums)-k >= gt {
+		return quick.Select(nums[gt:], k)
+	}
+	return pivot
 }
